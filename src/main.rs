8b744d334c03828a2dae71a57baec746674884b9
Mateusz Cegiełka <mateusz@cegla.net>
2017-11-13 18:42:26 +0100

    colored green/red progress bar

--- src/main.rs
+++ src/main.rs
@@ -103,10 +103,11 @@ fn run_test(args: &clap::ArgMatches, checker: Box<Checker>) {
             if print_success {
                 pb_interwrite!(pb, "{} {}", "TEST RUN SUCCESS".green().bold(), in_path.display());
             }
+            pb.inc1();
         } else {
             pb_interwrite!(pb, "{} {}", "TEST RUN FAILURE".red().bold(), in_path.display());
+            pb.inc2();
         }
-		pb.inc();
     }
 }
 
